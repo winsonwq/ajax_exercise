accc050948e1592cb2a7e9afe74a81204888d18f
Jerry <cabtravis@gmail.com>
2013-09-21 01:19:27 +0800

    code base

---
+++ README.md
@@ -0,0 +1,4 @@
+# js AJAX exercise
+#### use nodejs to lunch http server
+1.   npm install node-static
+2.   node nodeServe.js
---
+++ nodeServe.js
@@ -0,0 +1,9 @@
+static = require('node-static');
+
+var file = new static.Server('./');
+
+require('http').createServer(function(request, response) {
+    request.addListener('end', function() {
+          file.serve(request, response);
+            }).resume();
+}).listen(2080);
