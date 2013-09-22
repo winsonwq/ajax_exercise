c9c8be5da55909f5aadaa82e81f75d26146f36f8
Jerry <cabtravis@gmail.com>
2013-09-22 17:19:10 +0800

    zhihao changed the logic of getJSONS, what means this: we don't know the sequence of JSON receving, so we should use index of RESULTS

---
+++ ajax.js
@@ -0,0 +1,71 @@
+function getJSONSync() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "json.json", false);// false means sync way.
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send();// the process will hold on, waiting for the xml response
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        document.getElementById("display").innerHTML = "from asyn ajax request: " + xmlhttp.response;
+    }
+}
+
+function getJSONAsync() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "json.json", true);// if use true , add callback to xmlhtml.onreadystatechange = function(){...}
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("display").innerHTML = "from sync ajax request: " + xmlhttp.response;
+            console.log("done");
+        }
+    }
+    xmlhttp.send();
+}
+
+/* getJSONObject(url) */
+/* getJSONObject(url, params) */
+/* getJSONObject(url, callback) */
+/* getJSONObject(url, params, callback) */
+function getJSONObject(url, params, callback) {
+    if (arguments.length < 1) throw new Error("need the url for JSON");
+    var xmlhttp = new XMLHttpRequest();
+    var hasParams = true;
+    if (typeof params === "function" || arguments.length < 2) hasParams = false;
+    if (hasParams) {
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.send(params);
+    } else {
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send(null);
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var jsonObject;
+            if (!JSON) {
+                jsonObject = eval('(' + xmlhttp.responseText + ')');
+            } else {
+                jsonObject = JSON.parse(xmlhttp.responseText);
+            }
+
+            if (typeof params === "function") {
+                callback = params;
+            }
+            callback && callback.call(this,jsonObject);
+        }
+    }
+}
+
+function getJSONs() {
+    var results = [];
+    var args = Array.prototype.slice.call(arguments);
+    var callback = args.pop();
+
+    args.forEach(function (val, key) {
+        getJSONObject(val, function (jsonObject) {
+            results[key] = jsonObject;
+            if (results.length == args.length) {
+                callback && callback.call(this, results);
+            }
+        });
+    })
+}
